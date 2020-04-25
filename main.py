a4e2bb7e1b3ced72115eaf54023264a0404eeedb
Dipanshu Pandey <42908589+callmepandey@users.noreply.github.com>
2020-04-25 20:49:00 +0530

    Update main.py
    Added New Feature , If city not in Open weather Database. Based On The JSON response.

--- main.py
+++ main.py
@@ -11,26 +11,28 @@ url = main_url + "appid=" + api_key + "&q=" + city #It adds up city and api to f
 raw = requests.get(url)
 
 report = raw.json()
-
-temp=report["main"]["temp"]
-tempfeels=report["main"]["feels_like"]
-humidity=report["main"]["humidity"]
-temp = temp - 273.15
-tempfeels = tempfeels - 273.15
-timezone= report["timezone"]
-if timezone < 0:
-    hrs = timezone // 3600
-    min = -(timezone - hrs * 3600) // 60
+if report["cod"]==200:
+    temp=report["main"]["temp"]
+    tempfeels=report["main"]["feels_like"]
+    humidity=report["main"]["humidity"]
+    temp = temp - 273.15
+    tempfeels = tempfeels - 273.15
+    timezone= report["timezone"]
+    if timezone < 0:
+        hrs = timezone // 3600
+        min = -(timezone - hrs * 3600) // 60
+    else:
+        hrs=timezone//3600
+        min=(timezone - hrs*3600)//60
+    tempf= temp * 9/5 + 32
+    tempfeelsf= tempfeels * 9/5 + 32
+    print("Tempreture       ",round(temp,2)," °C  or ",round(tempf,2)," °F")
+    print("Tempreture Feels ",round(tempfeels,2)," °C  or ",round(tempfeelsf,2)," °F")
+    if tempfeels > 38:
+        print(" Tempreture May Cause Your Beautiful Skin Burn, Avoid Going Outside without Cover and Sunglasses ")
+    print("Humidity         ",humidity," % ")
+    if humidity > 85:
+        print("Its Likely to Rain Better take Umbrella with You")
+    print("Time Zone ",hrs,"Hour",min,"Mins GMT ")
 else:
-    hrs=timezone//3600
-    min=(timezone - hrs*3600)//60
-tempf= temp * 9/5 + 32
-tempfeelsf= tempfeels * 9/5 + 32
-print("Tempreture       ",round(temp,2)," °C  or ",round(tempf,2)," °F")
-print("Tempreture Feels ",round(tempfeels,2)," °C  or ",round(tempfeelsf,2)," °F")
-if tempfeels > 38:
-    print(" Tempreture May Cause Your Beautiful Skin Burn, Avoid Going Outside without Cover and Sunglasses ")
-print("Humidity         ",humidity," % ")
-if humidity > 85:
-    print("Its Likely to Rain Better take Umbrella with You")
-print("Time Zone ",hrs,"Hour",min,"Mins GMT ")
+    print("City Not Found In Database")
